c093e3d77fc5f71871ec1fd60d7190c0983f39cd
NtdChnn <69079640+NtdChnn@users.noreply.github.com>
2020-08-12 12:28:26 +0700

    finish

--- ChallengeW102/Source.cpp
+++ ChallengeW102/Source.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int num1,num2;
+	int num2,num1;
 	scanf_s("%d", &num1);
 	scanf_s("%d", &num2);
 	printf("%d + %d = %d\n%d - %d = %d\n%d x %d = %d\n%d / %d = %d",num1,num2,num1+num2,num1,num2,num1-num2,num1,num2,num1*num2,num1,num2,num1/num2 );
